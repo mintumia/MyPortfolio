82768da0ded05b210bafd7c75d3538961747a470
Md. Mintu Mia <mintumia8@gmail.com>
2024-01-07 21:06:00 +0600

    some change

--- resources/js/Components/MintuAvatarCard.vue
+++ resources/js/Components/MintuAvatarCard.vue
@@ -10,32 +10,36 @@ import {onMounted} from "vue";
 
 <template>
     <div class="m-2 mx-auto w-full">
-        <div class="inline-block items-center content-center justify-center mx-auto max-w-[400px] bg-blue-600 m-1 p-2 text-center rounded-lg shadow-lg shadow-black ">
-            <img class="rounded-full mx-auto" :src="$page.url+'assets/avatar.png'" alt="avatar">
+        <div class="relative inline-block items-center content-center justify-center mx-auto max-w-[400px] bg-blue-200 m-1 p-1 text-center rounded-lg shadow-lg shadow-black overflow-hidden z-20">
+           <span class="absolute left-0 top-0 bg-gradient-to-br to-blue-200 from-gray-900 from-25% to-45%  w-full h-full animate-[spin_5s_linear_infinite] z-30"></span>
+<!--            animate-[spin_5s_linear_infinite]-->
+             <div class="relative w-full h-full bg-blue-600 rounded-lg z-40">
+                 <img class="rounded-full mx-auto" :src="$page.url+'assets/avatar.png'" alt="avatar">
 
-            <h2 class="text-2xl font-semibold p-3 m-2">Md. Mintu Mia</h2>
-            <h3 class="text-xl font-semibold p-3 m-2 mb-4">PHP Laravel Full Stack Web Developer </h3>
-            <hr/>
+                 <h2 class="text-2xl font-semibold p-3 m-2">Md. Mintu Mia</h2>
+                 <h3 class="text-xl font-semibold p-3 m-2 mb-4">PHP Laravel Full Stack Web Developer </h3>
+                 <hr/>
 
-            <div class="flex justify-center items-center content-center">
-
-
-                <Link :href="route('about')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
-                    icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
-                <span> | </span>
-
-                <Link :href="route('home')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
-                    icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
-                <span> | </span>
-                <Link :href="route('service')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
-                    icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
-                <span> | </span>
-                <Link :href="route('contact')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
-                    icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
+                 <div class="flex justify-center items-center content-center">
 
 
-            </div>
+                     <Link :href="route('about')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
+                         icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
+                     <span> | </span>
 
+                     <Link :href="route('home')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
+                         icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
+                     <span> | </span>
+                     <Link :href="route('service')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
+                         icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
+                     <span> | </span>
+                     <Link :href="route('contact')" class="flex items-center text-lg p-2"><span class="mx-1"><Icon
+                         icon="skill-icons:vuejs-light"/></span><span>Github</span></Link>
+
+
+                 </div>
+
+             </div>
 
         </div>
 
--- resources/js/Components/MintuNavbar.vue
+++ resources/js/Components/MintuNavbar.vue
@@ -11,19 +11,20 @@ const navToggle = ref(false);
 <template>
 <div>
    <nav class="bg-slate-100">
-<div class="max-md:flex-wrap md:flex items-center justify-between content-center mx-auto">
+<div class="max-md:flex-wrap md:flex md:max-w-5xl items-center justify-between content-center mx-auto">
 
-   <div class="flex max-md:w-full items-center justify-between bg-amber-200 px-6">
+   <div class="flex max-md:w-full max-md:min-w-max items-center justify-between bg-amber-200 px-6">
        <div class="text-2xl font-bold mx-3 my-2">Icon</div>
-       <div class="max-md:flex md:hidden opacity-75 text-left"><button @click="navToggle = !navToggle" class="p-3"><span v-if="!navToggle"><icon icon="bi:grid-3x3-gap-fill" /> </span> <span v-else>X</span></button></div>
+       <div class="max-md:flex md:hidden text-left"><button @click="navToggle = !navToggle" class="p-3 opacity-75"><span v-if="!navToggle"><icon icon="material-symbols:menu-rounded" /> </span>
+           <span v-else> <icon icon="material-symbols:close-rounded"/>        </span></button></div>
    </div>
 
 
         <ul :class="navToggle ?'max-md:flex-wrap':'max-md:hidden'" class="md:flex items-center justify-center content-center mx-auto">
-            <li class="mx-3 my-2 border-b-2 text-center"><Link :href="route('home')">Home</Link></li>
-            <li class="mx-3 my-2 border-b-2 text-center"><Link :href="route('about')">About</Link></li>
-            <li class="mx-3 my-2 border-b-2 text-center"><Link :href="route('service')">Service</Link></li>
-            <li class="mx-3 my-2 border-b-2 text-center"><Link :href="route('contact')">Contact</Link></li>
+            <li class="mx-3 my-2 max-md:border-b-2 text-center"><Link :href="route('home')">Home</Link></li>
+            <li class="mx-3 my-2 max-md:border-b-2 text-center"><Link :href="route('about')">About</Link></li>
+            <li class="mx-3 my-2 max-md:border-b-2 text-center"><Link :href="route('service')">Service</Link></li>
+            <li class="mx-3 my-2 max-md:border-b-2 text-center"><Link :href="route('contact')">Contact</Link></li>
 
 
         </ul>
