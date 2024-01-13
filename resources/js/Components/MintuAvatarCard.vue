75b425b12b10fe71b1f655dbd2414fc203b208fe
PROPERTY\214843 <mintumia8@gmail.com>
2024-01-13 18:34:10 +0600

    Animation Added

--- resources/js/Components/MintuAvatarCard.vue
+++ resources/js/Components/MintuAvatarCard.vue
@@ -10,8 +10,8 @@ import {onMounted} from "vue";
 
 <template>
     <div class="m-2 mx-auto w-full">
-        <div class="relative inline-block items-center content-center justify-center mx-auto max-w-[400px] bg-blue-200 m-1 p-1 text-center rounded-lg shadow-lg shadow-black overflow-hidden z-20">
-           <span class="absolute left-0 top-0 bg-gradient-to-br to-blue-200 from-gray-900 from-25% to-45%  w-full h-full animate-[spin_5s_linear_infinite] z-30"></span>
+        <div class="relative inline-block items-center content-center justify-center mx-auto max-w-[400px] bg-slate-600 m-1 p-1 text-center rounded-lg shadow-lg shadow-black overflow-hidden z-20">
+           <span class="absolute origin-top-left  left-1/2 top-1/2 bg-gradient-to-r to-slate-600 from-cyan-300 from-10% to-85%  w-1/2 h-3/4 animate-[spin_10s_linear_infinite] z-30"></span>
 <!--            animate-[spin_5s_linear_infinite]-->
              <div class="relative w-full h-full bg-blue-600 rounded-lg z-40">
                  <img class="rounded-full mx-auto" :src="$page.url+'assets/avatar.png'" alt="avatar">
